6fc543bd518ce7e9bada73193a34fd6b995ad9f1
Aryan0393 <120777128+Aryan0393@users.noreply.github.com>
2025-06-25 18:20:49 +0530

    Update Results.tsx

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,4 +1,3 @@
-
 import { useLocation, useNavigate } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
